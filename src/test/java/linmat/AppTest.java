a0fa90227946cf34676267c5b23564d5bc77429a
lukago <golebiewski96@gmail.com>
2019-12-01 20:22:28 +0100

    lab 1

---
+++ src/test/java/linmat/AppTest.java
@@ -0,0 +1,13 @@
+package linmat;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+class AppTest {
+
+    @Test
+    void shouldAnswerWithTrue() {
+        assertTrue(true);
+    }
+}
